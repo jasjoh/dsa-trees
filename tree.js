21964670efdb4c3fb35b18d7e5529d30b738c988
Jason Johnson <jasjoh@live.com>
2023-10-04 11:20:51 -0700

    implemented sumValues() for trees

--- tree.js
+++ tree.js
@@ -11,7 +11,20 @@ class TreeNode {
   /** sumValues(): add up all values of invoking node and its children.
    * Returns sum as an integer. */
   sumValues() {
+    let toVisitStack = [this];
+    let sumValues = 0;
 
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      sumValues += current.val;
+
+      for (let child of current.children) {
+        toVisitStack.push(child)
+      }
+    }
+
+    return sumValues;
   }
 
   /** countEvens(): starting from the invoking node and moving through its
@@ -36,18 +49,18 @@ class Tree {
 
   /** sumValues(): add up all values in the tree. */
   sumValues() {
-
+    return this.root.sumValues();
   }
 
   /** countEvens(): count all nodes in the tree that have even values. */
   countEvens() {
-
+    return this.root.countEvens();
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
   numGreater(lowerBound) {
-
+    return this.root.numGreater(lowerBound);
   }
 }
 
