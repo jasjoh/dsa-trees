664064f188018f572a78d653e85a720216f66882
Jason Johnson <jasjoh@live.com>
2023-10-04 12:30:07 -0700

    added recursive versions of trees

--- tree.js
+++ tree.js
@@ -107,62 +107,36 @@ class TreeNode {
   /** sumValues(): add up all values of invoking node and its children.
    * Returns sum as an integer. */
   sumValues() {
-    let toVisitStack = [this];
-    let sumValues = 0;
-
-    while (toVisitStack.length) {
-      let current = toVisitStack.pop();
-
-      sumValues += current.val;
-
-      for (let child of current.children) {
-        toVisitStack.push(child)
-      }
+    let sum = 0;
+    sum += this.val;
+    for (let child of this.children) {
+      sum += child.sumValues()
     }
-
-    return sumValues;
+    return sum;
   }
 
   /** countEvens(): starting from the invoking node and moving through its
    * children, count how many nodes have even values. Returns that count as
    * an integer. */
   countEvens() {
-    let toVisitStack = [this];
-    let evensCount = 0;
-
-    while(toVisitStack.length){
-      let current = toVisitStack.pop();
-
-      if(current.val % 2 === 0){
-        evensCount++;
-      }
-
-      for(let child of current.children){
-        toVisitStack.push(child);
-      }
+    let count = 0;
+    if (this.val % 2 === 0) { count++; }
+    for (let child of this.children) {
+      count += child.countEvens();
     }
-    return evensCount;
+    return count;
   }
 
   /** numGreater(lowerBound): starting from the invoking node and moving through
    * its children, return a count of the number of nodes whose value is greater
    * than lowerBound. */
   numGreater(lowerBound){
-    let toVisitStack = [this];
-    let greaterCount = 0;
-
-    while(toVisitStack.length){
-      let current = toVisitStack.pop();
-
-      if(current.val > lowerBound){
-        greaterCount++;
-      }
-
-      for(let child of current.children){
-        toVisitStack.push(child);
-      }
+    let count = 0;
+    if (this.val > lowerBound) { count++; }
+    for (let child of this.children) {
+      count += child.numGreater(lowerBound);
     }
-    return greaterCount;
+    return count;
   }
 }
 
@@ -173,18 +147,27 @@ class Tree {
 
   /** sumValues(): add up all values in the tree. */
   sumValues() {
-    return this.root === null? this.root.sumValues() : 0;
+    // base case: this.children.length === 0
+    // add itself
+    // for every child, call sumValues() add result to sum
+    let sum = 0;
+    sum += this.root === null ? 0 : this.root.sumValues();
+    return sum;
   }
 
   /** countEvens(): count all nodes in the tree that have even values. */
   countEvens() {
-    return this.root === null? this.root.countEvens(): 0;
+    let count = 0;
+    if (this.root !== null) { count += this.root.countEvens(); }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
   numGreater(lowerBound) {
-    return this.root === null? this.root.numGreater(lowerBound): 0;
+    let count = 0;
+    if (this.root !== null) { count += this.root.numGreater(lowerBound); }
+    return count;
   }
 }
 
